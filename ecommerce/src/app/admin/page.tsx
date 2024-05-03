321c890928adc82caf70889ba748aa45e832d73c
RafaelTomazini <rafaelnt83@gmail.com>
2024-05-03 10:36:07 -0300

    added dashboard card

--- ecommerce/src/app/admin/page.tsx
+++ ecommerce/src/app/admin/page.tsx
@@ -1,3 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { DashBoardCardProps } from "./type";
+
+function DashBoardCard({ title, subtitle, description }: DashBoardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>{description}</CardContent>
+    </Card>
+  );
+}
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashBoardCard
+        title={"Sales"}
+        subtitle={"Sales Desc"}
+        description={"This is the description"}
+      />
+      <DashBoardCard
+        title={"Sales"}
+        subtitle={"Sales Desc"}
+        description={"This is the description"}
+      />
+      <DashBoardCard
+        title={"Sales"}
+        subtitle={"Sales Desc"}
+        description={"This is the description"}
+      />
+    </div>
+  );
 }
---
+++ ecommerce/src/app/admin/type.ts
@@ -0,0 +1,5 @@
+export type DashBoardCardProps = {
+  title: string;
+  subtitle: string;
+  description: string;
+};
